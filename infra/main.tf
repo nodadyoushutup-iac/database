1552cc1d1bb12be482aa99a63a87f7e911ac7566
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-21 23:59:39 +0000

    [WIP] Database

--- infra/main.tf
+++ infra/main.tf
@@ -1,28 +1,28 @@
-module "database" {
-  source  = "spacelift.io/nodadyoushutup/virtual_machine/proxmox"
-  # version = "1.0.0"
-  vm_id = 101
-  name = "database"
-  description = "Database services"
-  tags = ["terraform", "ubuntu", "database"]
-  disk = {
-    size = 50
-  }
-  memory = {
-    dedicated = 4096
-  }
-  cpu = {
-    cores = 2
-  }
-  initialization = {
-    ip_config = {
-      ipv4 = {
-        address = "192.168.1.101/24"
-        gateway = "192.168.1.1"
-      }
-    }
-  }
-}
+# module "database" {
+#   source  = "spacelift.io/nodadyoushutup/virtual_machine/proxmox"
+#   # version = "1.0.0"
+#   vm_id = 101
+#   name = "database"
+#   description = "Database services"
+#   tags = ["terraform", "ubuntu", "database"]
+#   disk = {
+#     size = 50
+#   }
+#   memory = {
+#     dedicated = 4096
+#   }
+#   cpu = {
+#     cores = 2
+#   }
+#   initialization = {
+#     ip_config = {
+#       ipv4 = {
+#         address = "192.168.1.101/24"
+#         gateway = "192.168.1.1"
+#       }
+#     }
+#   }
+# }
 
 resource "spacelift_context_attachment" "config_database" {
   count = local.component.database ? 1 : 0
